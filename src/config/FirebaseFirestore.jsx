67e7e2c7c8972f5f896b86fc08378c19847fed85
Laurentius <laurentiuskevin44@gmail.com>
2023-02-09 12:06:29 +0700

    change logic

--- src/config/FirebaseFirestore.jsx
+++ src/config/FirebaseFirestore.jsx
@@ -139,72 +139,53 @@ export const getAllCustomerStatistics = async (pageLimit = 12) => {
 };
 
 export const storeViewStatistics = async () => {
-  let savedAllView = 0,
-    overallView = 0;
+  const now = new Date(),
+    docsID = moment().format("YYYY-MM-DD");
+  let overallView = 0;
 
   const pages = await getAllPages();
-  const allView = await getAllView(0);
 
   pages.forEach((item) => {
     overallView += item.data().totalView ?? 0;
   });
 
-  allView.forEach((item) => {
-    savedAllView += item.data().totalView ?? 0;
-  });
-
-  let hourlyView = overallView - savedAllView;
-  hourlyView = hourlyView <= 0 ? 0 : hourlyView;
-
-  await setDoc(doc(firebaseFirestore, "pages-view", crypto.randomUUID()), {
-    totalView: hourlyView,
-    timestamp: new Date(),
-  });
-
-  await setDoc(doc(firebaseFirestore, "pages-view", "total-view"), {
-    view: overallView,
+  await setDoc(doc(firebaseFirestore, "pages-view", docsID), {
+    totalView: overallView,
+    timestamp: now,
   });
 
   return {
-    hourlyView: {
-      timestamp: new Date(),
-      totalView: hourlyView,
+    statistics: {
+      id: docsID,
+      data: {
+        timestamp: now,
+        totalView: overallView,
+      }
     },
-    allPageView: savedAllView,
   };
 };
 
 export const storeCustomerStatistics = async () => {
-  let savedTotalCustomer = 0,
-    overallCustomer = 0;
+  const now = new Date(),
+    docsID = moment().format("YYYY-MM");
 
-  const pageRef = collection(firebaseFirestore, "customer-monthly");
-  const q = query(pageRef, orderBy("timestamp", "desc"));
-  let savedStatistics = await getDocs(q);
-  savedStatistics.forEach((item) => {
-    savedTotalCustomer += item.data().totalCustomer ?? 0;
-  });
-
-  overallCustomer = (await getAllProfile()).size;
-
-  let monthlyCustomer = overallCustomer - savedTotalCustomer;
-  monthlyCustomer = monthlyCustomer <= 0 ? 0 : monthlyCustomer;
+  const overallCustomer = (await getAllProfile()).size;
 
   await setDoc(
-    doc(firebaseFirestore, "customer-monthly", crypto.randomUUID()),
+    doc(firebaseFirestore, "customer-monthly", docsID),
     {
-      totalCustomer: monthlyCustomer,
-      timestamp: new Date(),
+      totalCustomer: overallCustomer,
+      timestamp: now,
     }
   );
-  // await setDoc(doc(firebaseFirestore, "customer-monthly", "total-customer"), {
-  //   view: overallCustomer,
-  // });
 
   return {
     statistics: {
-      timestamp: new Date(),
-      totalCustomer: monthlyCustomer,
+      id: docsID,
+      data: {
+        timestamp: now,
+        totalCustomer: overallCustomer,
+      }
     },
   };
 };
